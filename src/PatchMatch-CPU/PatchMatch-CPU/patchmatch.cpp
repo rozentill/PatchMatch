5e92c1043787eec7496b538e737ccf6e2c82c852
rozentill <rozentill@gmail.com>
2016-11-02 15:57:40 +0800

    modify cpu and gpu

---
+++ src/PatchMatch-CPU/PatchMatch-CPU/patchmatch.cpp
@@ -0,0 +1,452 @@
+
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+
+using namespace cv;
+using namespace std;
+
+const int patch_w = 3;
+int	pm_iters = 5;
+int rs_max = INT_MAX;
+
+#define XY_TO_INT(x, y) (((y)<<12)|(x))
+#define INT_TO_X(v) ((v)&((1<<12)-1))
+#define INT_TO_Y(v) ((v>>12)&((1<<12)-1))
+
+//l2 distance between two patches
+int dist(int ***a, int ***b, int ax, int ay, int bx, int by, int cutoff = INT_MAX) {
+	int ans = 0;
+	for (int dy = 0; dy < patch_w; dy++) {
+		for (int dx = 0; dx < patch_w; dx++) {
+			int dr = a[ay + dy][ax + dx][2] - b[by + dy][bx + dx][2];
+			int dg = a[ay + dy][ax + dx][1] - b[by + dy][bx + dx][1];
+			int db = a[ay + dy][ax + dx][0] - b[by + dy][bx + dx][0];
+			ans += dr*dr + dg*dg + db*db;
+		}
+		if (ans >= cutoff) { return cutoff; }
+	}
+	return ans;
+}
+
+void improve_guess(int *** a, int *** b, int ax, int ay, int &xbest, int &ybest, int &dbest, int bx, int by) {
+	int d_cpu = 0;
+
+	d_cpu = dist(a, b, ax, ay, bx, by, dbest);
+	if (d_cpu < dbest) {
+		dbest = d_cpu;
+		xbest = bx;
+		ybest = by;
+	}
+}
+
+//get the approximate nearest neighbor and set it into ann
+void patchmatch(Mat a, Mat b, unsigned int **&ann, int **&annd) {
+
+	/* Initialize with random nearest neighbor field (NNF). */
+	int ans = 0;
+	int aew = a.cols - patch_w + 1, aeh = a.rows - patch_w + 1;       /* Effective width and height (possible upper left corners of patches). */
+	int bew = b.cols - patch_w + 1, beh = b.rows - patch_w + 1;
+
+	int *** a_pixel = new int **[a.rows];//set the rgb value from matrix a in a_pixel
+	int *** b_pixel = new int **[b.rows];
+	ann = new unsigned int *[a.rows];
+	annd = new int *[a.rows];
+
+	// initialize ann, annd ,a_pixel, b_pixel
+	for (int i = 0; i < a.rows; i++)
+	{
+		ann[i] = new unsigned int[a.cols];
+		annd[i] = new int[a.cols];
+		a_pixel[i] = new int*[a.cols];
+		memset(ann[i], 0, a.cols);
+		memset(annd[i], 0, a.cols);
+		for (int j = 0; j < a.cols; j++)
+		{
+			Vec3b ai = a.at<Vec3b>(i, j);
+			a_pixel[i][j] = new int[3];
+			a_pixel[i][j][0] = (int)ai.val[0];
+			a_pixel[i][j][1] = (int)ai.val[1];
+			a_pixel[i][j][2] = (int)ai.val[2];
+		}
+	}
+
+	for (int i = 0; i < b.rows; i++)
+	{
+		b_pixel[i] = new int*[b.cols];
+		for (int j = 0; j < b.cols; j++)
+		{
+			Vec3b bi = b.at<Vec3b>(i, j);
+			b_pixel[i][j] = new int[3];
+			b_pixel[i][j][0] = (int)bi.val[0];
+			b_pixel[i][j][1] = (int)bi.val[1];
+			b_pixel[i][j][2] = (int)bi.val[2];
+		}
+	}
+
+	for (int ay = 0; ay < aeh; ay++) {
+		for (int ax = 0; ax < aew; ax++) {
+			int bx = rand() % bew;
+			int by = rand() % beh;
+
+			//dist_gpu<<<1, threadsPerBlock >>>(dev_a, dev_b, dev_params);
+			ann[ay][ax] = XY_TO_INT(bx, by);
+			annd[ay][ax] = dist(a_pixel, b_pixel, ax, ay, bx, by);
+			//cout << "ann :" << ann[ay][ax] << ", annd :" << annd[ay][ax] << endl;
+
+		}
+	}
+	cout << "Finished randomly choosing nearest neighbor patches." << endl;
+	cout << "Now begin iteration..." << endl;
+	for (int iter = 0; iter < pm_iters; iter++) {
+		cout << "The iteration " << iter + 1 << "..." << endl;
+		/* In each iteration, improve the NNF, by looping in scanline or reverse-scanline order. */
+		int ystart = 0, yend = aeh, ychange = 1;
+		int xstart = 0, xend = aew, xchange = 1;
+		if (iter % 2 == 1) {
+			xstart = xend - 1; xend = -1; xchange = -1;
+			ystart = yend - 1; yend = -1; ychange = -1;
+		}
+		for (int ay = ystart; ay != yend; ay += ychange) {
+			for (int ax = xstart; ax != xend; ax += xchange) {
+				/* Current (best) guess. */
+				unsigned int v = ann[ay][ax];
+				int xbest = INT_TO_X(v), ybest = INT_TO_Y(v);
+				int dbest = annd[ay][ax];
+
+				/* Propagation: Improve current guess by trying instead correspondences from left and above (below and right on odd iterations). */
+				if ((unsigned)(ax - xchange) < (unsigned)aew && (ax - xchange) >= 0) {
+					int vp = ann[ay][ax - xchange];
+					int xp = INT_TO_X(vp) + xchange, yp = INT_TO_Y(vp);
+					if ((unsigned)xp < (unsigned)bew) {
+
+						improve_guess(a_pixel, b_pixel, ax, ay, xbest, ybest, dbest, xp, yp);
+					}
+				}
+
+				if ((unsigned)(ay - ychange) < (unsigned)aeh && (ay - ychange) >= 0) {
+					int vp = ann[ay - ychange][ax];
+					int xp = INT_TO_X(vp), yp = INT_TO_Y(vp) + ychange;
+					if ((unsigned)yp < (unsigned)beh) {
+
+						improve_guess(a_pixel, b_pixel, ax, ay, xbest, ybest, dbest, xp, yp);
+
+					}
+				}
+
+				/* Random search: Improve current guess by searching in boxes of exponentially decreasing size around the current best guess. */
+				int rs_start = rs_max;
+				if (rs_start > MAX(b.cols, b.rows)) { rs_start = MAX(b.cols, b.rows); }
+				for (int mag = rs_start; mag >= 1; mag /= 2) {
+					/* Sampling window */
+					int xmin = MAX(xbest - mag, 0), xmax = MIN(xbest + mag + 1, bew);
+					int ymin = MAX(ybest - mag, 0), ymax = MIN(ybest + mag + 1, beh);
+					int xp = xmin + rand() % (xmax - xmin);
+					int yp = ymin + rand() % (ymax - ymin);
+
+					improve_guess(a_pixel, b_pixel, ax, ay, xbest, ybest, dbest, xp, yp);
+
+				}
+
+				ann[ay][ax] = XY_TO_INT(xbest, ybest);
+				annd[ay][ax] = dbest;
+			}
+		}
+	}
+
+}
+
+int dist_p(Mat a, Mat b, int ax, int ay, int bx, int by) {
+	Vec3b ai = a.at<Vec3b>(ay, ax);
+	Vec3b bi = b.at<Vec3b>(by, bx);
+	int dr = abs(ai.val[2] - bi.val[2]);
+	int dg = abs(ai.val[1] - bi.val[1]);
+	int db = abs(ai.val[0] - bi.val[0]);
+	return dr*dr + dg*dg + db*db;
+}
+
+Mat reconstruct_flow(Mat a, Mat b, unsigned int ** ann, int patch_w) {
+	Mat c;
+	a.copyTo(c);
+
+	int aew = a.cols - patch_w + 1, aeh = a.rows - patch_w + 1;;
+
+	for (int ay = 0; ay < aeh; ay++) {
+		for (int ax = 0; ax < aew; ax++)
+		{
+			unsigned int v = ann[ay][ax];
+			int xbest = INT_TO_X(v);
+			int ybest = INT_TO_Y(v);
+
+			Vec3b bi = b.at<Vec3b>(ybest, xbest);
+			c.at<Vec3b>(ay, ax).val[0] = (uchar)255 * ((float)xbest / b.cols);
+			c.at<Vec3b>(ay, ax).val[2] = 0;
+			c.at<Vec3b>(ay, ax).val[1] = (uchar)255 * ((float)ybest / b.rows);
+		}
+	}
+	return c;
+}
+
+/* nearest voting */
+Mat reconstruct(Mat a, Mat b, unsigned int ** ann) {
+	Mat a_recon;
+	a.copyTo(a_recon);
+	int aew = a.cols - patch_w + 1, aeh = a.rows - patch_w + 1;       /* Effective width and height (possible upper left corners of patches). */
+	int bew = b.cols - patch_w + 1, beh = b.rows - patch_w + 1;
+	int ystart = 0, yend = aeh, ychange = 1;
+	int xstart = 0, xend = aew, xchange = 1;
+	unsigned int ybest = 0, xbest = 0, v = 0;
+	//difference of pixel
+	unsigned int ** pnnd;
+	unsigned int ** pnn;
+	pnn = new unsigned int *[a.rows];
+	pnnd = new unsigned int *[a.rows];
+	for (int i = 0; i < a.rows; i++)
+	{
+		pnn[i] = new unsigned int[a.cols];
+		pnnd[i] = new unsigned int[a.cols];
+		memset(pnn[i], 0, a.cols);
+	}
+
+
+	for (int ay = 0; ay < aeh; ay++) {
+		for (int ax = 0; ax < aew; ax++) {
+
+			if (ay < aeh&&ax < aew)
+			{
+				pnn[ay][ax] = ann[ay][ax];
+				v = ann[ay][ax];
+				xbest = INT_TO_X(v);
+				ybest = INT_TO_Y(v);
+				pnnd[ay][ax] = dist_p(a, b, ax, ay, xbest, ybest);
+			}
+			else if (ay >= aeh&&ax < aew) {
+				v = ann[aeh - 1][ax];
+				xbest = INT_TO_X(v);
+				ybest = INT_TO_Y(v);
+				ybest += (ay - aeh + 1);
+				pnn[ay][ax] = XY_TO_INT(xbest, ybest);
+				pnnd[ay][ax] = dist_p(a, b, ax, ay, xbest, ybest);
+			}
+			else if (ay < aeh&&ax >= aew) {
+				v = ann[ay][aew - 1];
+				xbest = INT_TO_X(v);
+				ybest = INT_TO_Y(v);
+				xbest += (ax - aew + 1);
+				pnn[ay][ax] = XY_TO_INT(xbest, ybest);
+				pnnd[ay][ax] = dist_p(a, b, ax, ay, xbest, ybest);
+
+			}
+			else {
+				v = ann[aeh - 1][aew - 1];
+				xbest = INT_TO_X(v);
+				ybest = INT_TO_Y(v);
+				xbest += (ax - aew + 1);
+				ybest += (ay - aeh + 1);
+				pnn[ay][ax] = XY_TO_INT(xbest, ybest);
+				pnnd[ay][ax] = dist_p(a, b, ax, ay, xbest, ybest);
+			}
+
+		}
+	}
+
+
+	for (int ay = ystart; ay != yend; ay += ychange) {
+		for (int ax = xstart; ax != xend; ax += xchange) {
+			v = ann[ay][ax];
+			xbest = INT_TO_X(v);
+			ybest = INT_TO_Y(v);
+
+			for (int dy = 0; dy < patch_w; dy++) {
+				for (int dx = 0; dx < patch_w; dx++) {
+					if (pnnd[ay + dy][ax + dx]>dist_p(a, b, ax + dx, ay + dy, xbest + dx, ybest + dy)) {
+						pnn[ay + dy][ax + dx] = XY_TO_INT(xbest + dx, ybest + dy);
+						pnnd[ay + dy][ax + dx] = dist_p(a, b, ax + dx, ay + dy, xbest + dx, ybest + dy);
+					}
+
+				}
+
+			}
+		}
+	}
+
+	for (int ay = ystart; ay < a.rows; ay++) {
+		for (int ax = xstart; ax < a.cols; ax++)
+		{
+			v = pnn[ay][ax];
+			xbest = INT_TO_X(v);
+			ybest = INT_TO_Y(v);
+
+			Vec3b bi = b.at<Vec3b>(ybest, xbest);
+			a_recon.at<Vec3b>(ay, ax).val[2] = bi.val[2];
+			a_recon.at<Vec3b>(ay, ax).val[1] = bi.val[1];
+			a_recon.at<Vec3b>(ay, ax).val[0] = bi.val[0];
+		}
+	}
+	return a_recon;
+}
+
+/* average voting */
+Mat reconstruct_average(Mat a, Mat b, unsigned int ** ann) {
+	Mat a_recon;
+	a.copyTo(a_recon);
+	int aew = a.cols - patch_w + 1, aeh = b.rows - patch_w + 1;       /* Effective width and height (possible upper left corners of patches). */
+	int bew = b.cols - patch_w + 1, beh = b.rows - patch_w + 1;
+	int ystart = 0, yend = aeh, ychange = 1;
+	int xstart = 0, xend = aew, xchange = 1;
+	unsigned int ybest = 0, xbest = 0, v = 0;
+	//difference of pixel
+	unsigned int ** pnnd;
+	unsigned int ** pnn;
+	pnn = new unsigned int *[a.rows];
+	pnnd = new unsigned int *[a.rows];
+	for (int i = 0; i < a.rows; i++)
+	{
+		pnn[i] = new unsigned int[a.cols];
+		pnnd[i] = new unsigned int[a.cols];
+		memset(pnn[i], 0, a.cols);
+		memset(pnnd[i], 0, a.cols);
+
+	}
+
+
+	for (int ay = 0; ay < aeh; ay++) {
+		for (int ax = 0; ax < aew; ax++) {
+
+			if (ay < aeh&&ax < aew)
+			{
+				pnn[ay][ax] = ann[ay][ax];
+				v = ann[ay][ax];
+				xbest = INT_TO_X(v);
+				ybest = INT_TO_Y(v);
+				pnnd[ay][ax] = dist_p(a, b, ax, ay, xbest, ybest);
+			}
+			else if (ay >= aeh&&ax < aew) {
+				v = ann[aeh - 1][ax];
+				xbest = INT_TO_X(v);
+				ybest = INT_TO_Y(v);
+				ybest += (ay - aeh + 1);
+				pnn[ay][ax] = XY_TO_INT(xbest, ybest);
+				pnnd[ay][ax] = dist_p(a, b, ax, ay, xbest, ybest);
+			}
+			else if (ay < aeh&&ax >= aew) {
+				v = ann[ay][aew - 1];
+				xbest = INT_TO_X(v);
+				ybest = INT_TO_Y(v);
+				xbest += (ax - aew + 1);
+				pnn[ay][ax] = XY_TO_INT(xbest, ybest);
+				pnnd[ay][ax] = dist_p(a, b, ax, ay, xbest, ybest);
+
+			}
+			else {
+				v = ann[aeh - 1][aew - 1];
+				xbest = INT_TO_X(v);
+				ybest = INT_TO_Y(v);
+				xbest += (ax - aew + 1);
+				ybest += (ay - aeh + 1);
+				pnn[ay][ax] = XY_TO_INT(xbest, ybest);
+				pnnd[ay][ax] = dist_p(a, b, ax, ay, xbest, ybest);
+			}
+
+		}
+	}
+
+
+	for (int ay = ystart; ay != yend; ay += ychange) {
+		for (int ax = xstart; ax != xend; ax += xchange) {
+			v = ann[ay][ax];
+			xbest = INT_TO_X(v);
+			ybest = INT_TO_Y(v);
+
+			for (int dy = 0; dy < patch_w; dy++) {
+				for (int dx = 0; dx < patch_w; dx++) {
+					if (pnnd[ay + dy][ax + dx]>dist_p(a, b, ax + dx, ay + dy, xbest + dx, ybest + dy)) {
+						pnn[ay + dy][ax + dx] = XY_TO_INT(xbest + dx, ybest + dy);
+						pnnd[ay + dy][ax + dx] = dist_p(a, b, ax + dx, ay + dy, xbest + dx, ybest + dy);
+					}
+
+				}
+
+			}
+		}
+	}
+
+	for (int ay = ystart; ay < a.rows; ay++) {
+		for (int ax = xstart; ax < a.cols; ax++)
+		{
+			v = pnn[ay][ax];
+			xbest = INT_TO_X(v);
+			ybest = INT_TO_Y(v);
+
+			Vec3b bi = b.at<Vec3b>(ybest, xbest);
+			a_recon.at<Vec3b>(ay, ax).val[2] = bi.val[2];
+			a_recon.at<Vec3b>(ay, ax).val[1] = bi.val[1];
+			a_recon.at<Vec3b>(ay, ax).val[0] = bi.val[0];
+		}
+	}
+	return a_recon;
+}
+
+int main()
+{
+
+	string window_name = "reconstructed";
+	//string file_a, file_b;
+	string file_a = "bike_a.png";
+	string file_b = "bike_b.png";
+	string image_path = "D:/MSRA/Code/Project/Artistic-Filter/Artistic Filter/Artistic Filter/files/images/";
+
+
+	/*cout << "Please enter the filename of image A in the 'Image' folder :(E.g. a1.png) :";
+	cin >> file_a;
+	cout << "Please enter the filename of image B in the 'Image' folder :(E.g. b1.png) :";
+	cin >> file_b;*/
+	// define img matrix
+	//Mat a = imread("Image/" + file_a);
+	//Mat b = imread("Image/" + file_b);
+	Mat a = imread(image_path + file_a);
+	Mat b = imread(image_path + file_b);
+	Mat a_recon;
+	if (a.empty() || b.empty())
+	{
+		cout << "image cannot read!" << endl;
+		waitKey();
+		exit;
+	}
+
+	cout << "Images loaded." << endl;
+	// define and initialize ann and annd array
+	int **annd;
+	unsigned int**ann;
+
+	cout << "Now begin to find ann." << endl;
+	patchmatch(a, b, ann, annd);
+	cout << "Finished finding ann." << endl;
+	cout << "Now begin to reconstruct new image." << endl;
+
+	a_recon = reconstruct(a, b, ann);
+	cout << "Finish reconstruction of the new image." << endl;
+
+	Mat flowmap = reconstruct_flow(a, b, ann, patch_w);
+	namedWindow("flow", CV_WINDOW_AUTOSIZE);
+	imshow("flow", flowmap);
+
+	namedWindow(window_name, CV_WINDOW_AUTOSIZE);
+	imshow(window_name, a_recon);
+
+	namedWindow("a", CV_WINDOW_AUTOSIZE);
+	imshow("a", a);
+
+	namedWindow("b", CV_WINDOW_AUTOSIZE);
+	imshow("b", b);
+
+	imwrite(image_path+"bike_cpu_corner_nearest.png", a_recon);
+
+	waitKey();
+
+	destroyAllWindows();
+
+	return 0;
+}
+
---
+++ src/PatchMatch-GPU/PatchMatch-GPU/kernel.cu
@@ -0,0 +1,486 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "device_functions.h"
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <curand_kernel.h>
+
+using namespace cv;
+using namespace std;
+
+
+__host__ __device__ unsigned int XY_TO_INT(int x, int y) {
+	return ((y) << 12) | (x);
+}
+__host__ __device__ int INT_TO_X(unsigned int v) {
+	return (v)&((1 << 12) - 1);
+}
+__host__ __device__ int INT_TO_Y(unsigned int v) {
+	return (v >> 12)&((1 << 12) - 1);
+}
+
+__host__ __device__ int cuMax(int a, int b) {
+	if (a>b) {
+		return a;
+	}
+	else {
+		return b;
+	}
+}
+
+__host__ __device__ int cuMin(int a, int b) {
+	if (a<b) {
+		return a;
+	}
+	else {
+		return b;
+	}
+}
+
+
+__host__ int dist_p(Mat a, Mat b, int ax, int ay, int bx, int by) {
+	Vec3b ai = a.at<Vec3b>(ay, ax);
+	Vec3b bi = b.at<Vec3b>(by, bx);
+	int dr = abs(ai.val[2] - bi.val[2]);
+	int dg = abs(ai.val[1] - bi.val[1]);
+	int db = abs(ai.val[0] - bi.val[0]);
+	return dr*dr + dg*dg + db*db;
+}
+
+/* nearest voting */
+__host__ Mat reconstruct(Mat a, Mat b, unsigned int * ann, int patch_w) {
+	Mat c;
+	a.copyTo(c);
+	int aew = a.cols - patch_w + 1, aeh = b.rows - patch_w + 1;       /* Effective width and height (possible upper left corners of patches). */
+	int bew = b.cols - patch_w + 1, beh = b.rows - patch_w + 1;
+	int ystart = 0, yend = aeh, ychange = 1;
+	int xstart = 0, xend = aew, xchange = 1;
+	unsigned int ybest = 0, xbest = 0, v = 0;
+	//difference of pixel
+	int ** pnnd;
+	unsigned int ** pnn;
+	pnn = new unsigned int *[a.rows];
+	pnnd = new int *[a.rows];
+	for (int i = 0; i < a.rows; i++)
+	{
+		pnn[i] = new unsigned int[a.cols];
+		pnnd[i] = new int[a.cols];
+		memset(pnn[i], 0, a.cols);
+	}
+
+
+	for (int ay = 0; ay < a.rows; ay++) {
+		for (int ax = 0; ax < a.cols; ax++) {
+
+			if (ay < aeh&&ax < aew)
+			{
+				pnn[ay][ax] = ann[ay*aew + ax];
+				v = ann[ay*aew + ax];
+				xbest = INT_TO_X(v);
+				ybest = INT_TO_Y(v);
+				pnnd[ay][ax] = dist_p(a, b, ax, ay, xbest, ybest);
+			}
+			else if (ay >= aeh&&ax < aew) {
+				v = ann[(aeh - 1)*aew + ax];
+				xbest = INT_TO_X(v);
+				ybest = INT_TO_Y(v);
+				ybest += (ay - aeh + 1);
+				pnn[ay][ax] = XY_TO_INT(xbest, ybest);
+				pnnd[ay][ax] = dist_p(a, b, ax, ay, xbest, ybest);
+			}
+			else if (ay < aeh&&ax >= aew) {
+				v = ann[ay*aew + aew - 1];
+				xbest = INT_TO_X(v);
+				ybest = INT_TO_Y(v);
+				xbest += (ax - aew + 1);
+				pnn[ay][ax] = XY_TO_INT(xbest, ybest);
+				pnnd[ay][ax] = dist_p(a, b, ax, ay, xbest, ybest);
+
+			}
+			else {
+				v = ann[(aeh - 1)*aew + aew - 1];
+				xbest = INT_TO_X(v);
+				ybest = INT_TO_Y(v);
+				xbest += (ax - aew + 1);
+				ybest += (ay - aeh + 1);
+				pnn[ay][ax] = XY_TO_INT(xbest, ybest);
+				pnnd[ay][ax] = dist_p(a, b, ax, ay, xbest, ybest);
+			}
+
+		}
+	}
+
+
+	for (int ay = ystart; ay != yend; ay += ychange) {
+		for (int ax = xstart; ax != xend; ax += xchange) {
+			v = ann[ay*aew + ax];
+			xbest = INT_TO_X(v);
+			ybest = INT_TO_Y(v);
+
+			for (int dy = 0; dy < patch_w; dy++) {
+				for (int dx = 0; dx < patch_w; dx++) {
+					if (pnnd[ay + dy][ax + dx]>dist_p(a, b, ax + dx, ay + dy, xbest + dx, ybest + dy)) {
+						pnn[ay + dy][ax + dx] = XY_TO_INT(xbest + dx, ybest + dy);
+						pnnd[ay + dy][ax + dx] = dist_p(a, b, ax + dx, ay + dy, xbest + dx, ybest + dy);
+					}
+
+				}
+
+			}
+		}
+	}
+
+	for (int ay = ystart; ay < a.rows; ay++) {
+		for (int ax = xstart; ax < a.cols; ax++)
+		{
+			v = pnn[ay][ax];
+			xbest = INT_TO_X(v);
+			ybest = INT_TO_Y(v);
+
+			Vec3b bi = b.at<Vec3b>(ybest, xbest);
+			c.at<Vec3b>(ay, ax).val[2] = bi.val[2];
+			c.at<Vec3b>(ay, ax).val[1] = bi.val[1];
+			c.at<Vec3b>(ay, ax).val[0] = bi.val[0];
+		}
+	}
+	return c;
+}
+
+
+__host__ __device__ int dist(int * a, int * b, int a_rows, int a_cols, int b_rows, int b_cols, int ax, int ay, int bx, int by, int patch_w, int cutoff = INT_MAX) {
+
+	int ans = 0;
+	for (int dy = 0; dy < patch_w; dy++) {
+		for (int dx = 0; dx < patch_w; dx++) {
+			int dr = a[(ay + dy) * 3 * a_cols + (ax + dx) * 3 + 2] - b[(by + dy) * 3 * b_cols + (bx + dx) * 3 + 2];
+			int dg = a[(ay + dy) * 3 * a_cols + (ax + dx) * 3 + 1] - b[(by + dy) * 3 * b_cols + (bx + dx) * 3 + 1];
+			int db = a[(ay + dy) * 3 * a_cols + (ax + dx) * 3 + 0] - b[(by + dy) * 3 * b_cols + (bx + dx) * 3 + 0];
+			ans += dr*dr + dg*dg + db*db;
+		}
+		if (ans >= cutoff) { return cutoff; }
+	}
+	return ans;
+}
+
+__host__ void convertMatToArray(Mat mat, int *& arr) {
+	arr = new int[mat.rows*mat.cols * 3];
+	for (int i = 0; i < mat.rows; i++)
+	{
+		for (int j = 0; j <mat.cols; j++)
+		{
+			Vec3b rgb = mat.at<Vec3b>(i, j);
+			arr[i*mat.cols * 3 + j * 3 + 0] = rgb.val[0];
+			arr[i*mat.cols * 3 + j * 3 + 1] = rgb.val[1];
+			arr[i*mat.cols * 3 + j * 3 + 2] = rgb.val[2];
+		}
+	}
+}
+
+__host__ void convertArrayToMat(Mat & mat, int *arr) {
+	for (int i = 0; i < mat.rows; i++)
+	{
+		for (int j = 0; j <mat.cols; j++)
+		{
+			mat.at<Vec3b>(i, j).val[0] = arr[i*mat.cols * 3 + j * 3 + 0];
+			mat.at<Vec3b>(i, j).val[1] = arr[i*mat.cols * 3 + j * 3 + 1];
+			mat.at<Vec3b>(i, j).val[2] = arr[i*mat.cols * 3 + j * 3 + 2];
+		}
+	}
+}
+
+__host__ void initialAnn(unsigned int *& ann, int *& annd, int aw, int ah, int bw, int bh, int aew, int aeh, int bew, int beh, int * a, int * b, int patch_w) {
+	for (int ay = 0; ay < aeh; ay++) {
+		for (int ax = 0; ax < aew; ax++) {
+			int bx = rand() % bew;
+			int by = rand() % beh;
+
+			ann[ay*aew + ax] = XY_TO_INT(bx, by);
+			annd[ay*aew + ax] = dist(a, b, ah, aw, bh, bw, ax, ay, bx, by, patch_w);
+
+		}
+	}
+}
+
+__host__ void print(String string) {
+	cout << string;
+}
+
+__device__ void improve_guess(int * a, int * b, int a_rows, int a_cols, int b_rows, int b_cols, int ax, int ay, int &xbest, int &ybest, int &dbest, int xp, int yp, int patch_w) {
+	int d = 0;
+	d = dist(a, b, a_rows, a_cols, b_rows, b_cols, ax, ay, xp, yp, patch_w, dbest);
+
+	if (d < dbest) {
+		xbest = xp;
+		ybest = yp;
+		dbest = d;
+	}
+}
+
+__device__ float cuRand(unsigned int * seed) {//random number in cuda
+	unsigned long a = 16807;
+	unsigned long m = 2147483647;
+	unsigned long x = (unsigned long)* seed;
+	x = (a*x) % m;
+	*seed = (unsigned int)x;
+	return ((float)x / m);
+}
+
+/*get the approximate nearest neighbor and set it into ann
+********************************************************
+params: 7
+-----------------------------------------------
+0 - a_rows
+1 - a_cols
+2 - b_rows
+3 - b_cols
+4 - patch_w
+5 - pm_iter
+6 - rs_max
+*********************************************************/
+__global__ void patchmatch(int * a, int * b, unsigned int *ann, int *annd, int * params) {
+
+	int ax = blockIdx.x*blockDim.x + threadIdx.x;
+	int ay = blockIdx.y*blockDim.y + threadIdx.y;
+
+
+						//assign params
+	int a_rows = params[0];
+	int a_cols = params[1];
+	int b_rows = params[2];
+	int b_cols = params[3];
+	int patch_w = params[4];
+	int pm_iters = params[5];
+	int rs_max = params[6];
+
+	int aew = a_cols - patch_w + 1;
+	int aeh = a_rows - patch_w + 1;
+	int bew = b_cols - patch_w + 1;
+	int beh = b_rows - patch_w + 1;
+
+	
+
+	// for random number
+	unsigned int seed = ay*aew+ax;
+
+	if (ax < a_cols&&ay < a_rows) {
+
+		for (int iter = 0; iter < pm_iters; iter++) {
+			/* In each iteration, improve the NNF, by jumping flooding. */
+
+			/* Current (best) guess. */
+			unsigned int v = ann[ay*aew + ax];
+			int xbest = INT_TO_X(v), ybest = INT_TO_Y(v);
+			int dbest = annd[ay*aew + ax];
+
+			for (int jump = 8; jump > 0; jump /= 2) {
+
+
+
+				/* Propagation: Improve current guess by trying instead correspondences from left, right, up and downs. */
+				if ((ax - jump) >= 0)//left
+				{
+					unsigned int vp = ann[ay*aew + ax - jump];//the pixel coordinates in image b
+					int xp = INT_TO_X(vp) + jump, yp = INT_TO_Y(vp);//the propagated match from vp
+					if (xp < bew)
+					{
+						//improve guress
+						improve_guess(a, b, a_rows, a_cols, b_rows, b_cols, ax, ay, xbest, ybest, dbest, xp, yp, patch_w);
+					}
+				}
+
+				if ((ax + jump) < aew)//right
+				{
+					unsigned int vp = ann[ay*aew + ax + jump];//the pixel coordinates in image b
+					int xp = INT_TO_X(vp) - jump, yp = INT_TO_Y(vp);
+					if (xp >= 0)
+					{
+						//improve guress
+						improve_guess(a, b, a_rows, a_cols, b_rows, b_cols, ax, ay, xbest, ybest, dbest, xp, yp, patch_w);
+					}
+				}
+
+				if ((ay + jump) < aeh)//up
+				{
+					unsigned int vp = ann[(ay + jump)*aew + ax];//the pixel coordinates in image b
+					int xp = INT_TO_X(vp), yp = INT_TO_Y(vp) - jump;
+					if (yp >= 0)
+					{
+						//improve guress
+						improve_guess(a, b, a_rows, a_cols, b_rows, b_cols, ax, ay, xbest, ybest, dbest, xp, yp, patch_w);
+					}
+				}
+
+				if ((ay - jump) >= 0)//down
+				{
+					unsigned int vp = ann[(ay - jump)*aew + ax];//the pixel coordinates in image b
+					int xp = INT_TO_X(vp), yp = INT_TO_Y(vp) + jump;
+					if (yp < beh)
+					{
+						//improve guress
+						improve_guess(a, b, a_rows, a_cols, b_rows, b_cols, ax, ay, xbest, ybest, dbest, xp, yp, patch_w);
+					}
+				}
+				ann[ay*aew + ax] = XY_TO_INT(xbest, ybest);
+				annd[ay*aew + ax] = dbest;
+
+				__syncthreads();
+
+
+			}
+
+			/* Random search: Improve current guess by searching in boxes of exponentially decreasing size around the current best guess. */
+			int rs_start = rs_max;
+			if (rs_start > cuMax(b_cols, b_rows)) {
+				rs_start = cuMax(b_cols, b_rows);
+			}
+			for (int mag = rs_start; mag >= 1; mag /= 2) {
+				/* Sampling window */
+				int xmin = cuMax(xbest - mag, 0), xmax = cuMin(xbest + mag + 1, bew);
+				int ymin = cuMax(ybest - mag, 0), ymax = cuMin(ybest + mag + 1, beh);
+				int xp = xmin + (int)(cuRand(&seed)*(xmax - xmin)) % (xmax - xmin);
+				int yp = ymin + (int)(cuRand(&seed)*(ymax - ymin)) % (ymax - ymin);
+
+				improve_guess(a, b, a_rows, a_cols, b_rows, b_cols, ax, ay, xbest, ybest, dbest, xp, yp, patch_w);
+
+			}
+
+
+			ann[ay*aew + ax] = XY_TO_INT(xbest, ybest);
+			annd[ay*aew + ax] = dbest;
+			__syncthreads();
+
+		}
+	}
+}
+
+int main()
+{
+
+	/*** Definition and initialization ***/
+	string window_name = "reconstructed";
+	string file_a, file_b;
+	string test_a = "bike_a.png";
+	string test_b = "bike_b.png";
+	string image_path = "D:/MSRA/Code/Project/Artistic-Filter/Artistic Filter/Artistic Filter/files/images/";
+
+
+	Mat a, b, c;
+	int * newannd, * a_host, *b_host, *a_device, *b_device, *annd_host, *annd_device, *params_host, *params_device;
+	int a_size, b_size, aeh, aew, beh, bew;
+	unsigned int * ann_host, *ann_device;
+	unsigned int * newann;
+
+	const int patch_w = 7;
+	int pm_iters = 5;
+	int sizeOfParams = 7;
+	int rs_max = INT_MAX;
+	int sizeOfAnn;
+
+	/*** load files ***/
+
+	/*cout << "Please enter the filename of image A in the 'Image' folder :(E.g. a1.png) :";
+	cin >> file_a;
+	cout << "Please enter the filename of image B in the 'Image' folder :(E.g. b1.png) :";
+	cin >> file_b;
+
+	a = imread("Image/" + file_a);
+	b = imread("Image/" + file_b);*/
+
+
+	a = imread(image_path + test_a);
+	b = imread(image_path + test_b);
+	a_size = a.rows*a.cols * 3;
+	b_size = b.rows*b.cols * 3;
+	
+
+	a.copyTo(c);//initialize c
+
+	aew = a.cols - patch_w + 1;
+	aeh = a.rows - patch_w + 1;
+	bew = b.cols - patch_w + 1;
+	beh = b.rows - patch_w + 1;
+	sizeOfAnn = aew*aeh;
+
+	dim3 blocksPerGrid(aew / 32 + 1, aeh / 32 + 1, 1);
+	dim3 threadsPerBlock(32, 32, 1);
+
+
+	//judge whether it is empty
+	if (a.empty() || b.empty())
+	{
+		cout << "image cannot read!" << endl;
+		waitKey();
+		exit;
+	}
+
+	cout << "Images loaded." << endl;
+
+
+	/* initialization */
+	ann_host = (unsigned int *)malloc(sizeOfAnn * sizeof(unsigned int));
+	annd_host = (int *)malloc(sizeOfAnn * sizeof(int));
+	newann = (unsigned int *)malloc(sizeOfAnn * sizeof(unsigned int));
+	newannd = (int *)malloc(sizeOfAnn * sizeof(int));
+
+	params_host = (int *)malloc(sizeOfParams * sizeof(int));
+	params_host[0] = a.rows;
+	params_host[1] = a.cols;
+	params_host[2] = b.rows;
+	params_host[3] = b.cols;
+	params_host[4] = patch_w;
+	params_host[5] = pm_iters;
+	params_host[6] = rs_max;
+
+	convertMatToArray(a, a_host);
+	convertMatToArray(b, b_host);
+	initialAnn(ann_host, annd_host, a.cols, a.rows, b.cols, b.rows, aew, aeh, bew, beh, a_host, b_host, patch_w);
+
+	cudaMalloc(&a_device, a_size * sizeof(int));
+	cudaMalloc(&b_device, b_size * sizeof(int));
+	cudaMalloc(&annd_device, sizeOfAnn * sizeof(int));
+	cudaMalloc(&ann_device, sizeOfAnn * sizeof(unsigned int));
+	cudaMalloc(&params_device, sizeOfParams * sizeof(int));
+
+	cudaMemcpy(a_device, a_host, a_size * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(b_device, b_host, b_size * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(ann_device, ann_host, sizeOfAnn * sizeof(unsigned int), cudaMemcpyHostToDevice);
+	cudaMemcpy(annd_device, annd_host, sizeOfAnn * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(params_device, params_host, sizeOfParams * sizeof(int), cudaMemcpyHostToDevice);
+
+	print("Now begin to find ann.\n");
+
+	patchmatch<<<blocksPerGrid, threadsPerBlock>>>(a_device, b_device, ann_device, annd_device, params_device);
+
+	print((string)"Finished finding ann.\n" + (string)"Now begin to reconstruct new image.");
+
+	cudaMemcpy(newann, ann_device, sizeOfAnn * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(newannd, annd_device, sizeOfAnn * sizeof(int), cudaMemcpyDeviceToHost);
+
+	cudaFree(a_device);
+	cudaFree(b_device);
+	cudaFree(ann_device);
+	cudaFree(annd_device);
+	cudaFree(params_device);
+
+	c = reconstruct(a, b, newann, patch_w);
+	cout << "Finish reconstruction of the new image." << endl;
+
+	namedWindow(window_name, CV_WINDOW_AUTOSIZE);
+	imshow(window_name, c);
+
+	namedWindow("a", CV_WINDOW_AUTOSIZE);
+	imshow("a", a);
+
+	namedWindow("b", CV_WINDOW_AUTOSIZE);
+	imshow("b", b);
+
+	//imwrite("Image/result.png", c);
+
+	waitKey();
+
+	destroyAllWindows();
+
+	return 0;
+}
